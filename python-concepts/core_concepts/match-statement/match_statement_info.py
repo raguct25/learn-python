7117583927e4aa0adfb4eabdbbe52ec3955aa2ae
Ragu Subramaniyam <raguct25@gmail.com>
2025-05-20 21:11:08 +0530

    match statement #003

---
+++ python-concepts/core_concepts/match-statement/match_statement_info.py
@@ -0,0 +1,24 @@
+# Match-Statement 
+
+"""
+* Match statement is like a (javasript) switch case 
+* introduced python 3.10
+
+    Match Statement:
+
+        - Structure match - match the objects ( like : list, tuple, dict and custom objects also )
+            * it check keys and values
+            * if incorrect key or values it not execute
+        - Type checking - match the value types ( like : int, str, float etc ... )
+            * if define case int() and second bool() the bool value type choose as integer not run bool type
+            * if want check bool type should declare while bool() will run
+        - Value - match the value ( like: text, numbers, defined values )
+        - Guards - it condition check. condition check with passed values
+
+    * the default case not there. the default case is case _: the underscore is default case
+    * if any case fails the default case will execute
+
+
+
+"""
+
